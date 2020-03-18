7573d7d74fe83442b22c37f0595030b61f34d7a7
유동완 <ydw0113@naver.com>
2020-03-18 15:55:47 +0900

    스터디

--- heap02.java
+++ heap02.java
@@ -1,9 +1,34 @@
 package study;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class heap02 {
 	public static int heap02(int stock, int[] dates, int[] supplies, int k) {
+		PriorityQueue<Integer> pq;
+		int count = 0;
 		int answer = 0;
-		return answer;
+		int day = 0;
+		int tmp = 0;
+		pq = new PriorityQueue<>(Comparator.reverseOrder());
+		for (int i = 0; i < k; i++) {
+
+			if (stock == 0) {
+				int j = day;
+				while (j < dates.length && dates[j] <= i) {
+					pq.add(supplies[j]);
+					j++;
+				}
+				day = j;
+				count++;
+				stock += pq.poll();
+			}
+			if (stock + i >= k)
+				break;
+			stock--;
+		}
+		System.out.println(count);
+		return count;
 	}
 
 	public static void main(String[] args) {

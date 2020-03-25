7edcbb2febdde9e0215942963753f7e90df3075d
유동완 <ydw0113@naver.com>
2020-03-25 18:30:24 +0900

    스터디

--- Kakao03.java
+++ Kakao03.java
@@ -7,59 +7,70 @@ public class Kakao03 {
 		int answer = 0;
 		String s = "";
 		String s2 = "";
+		int index = 0;
+		boolean check = false;
 		LinkedList<String> list = new LinkedList<String>();
 		LinkedList<String> list2 = new LinkedList<String>();
 		for (int i = 0; i < str1.length(); i++) {
-			char t = str1.charAt(i);
-			if (65 <= t && t <= 90) {
-				s += str1.substring(i, i + 1);
-			} else if (97 <= t && t <= 122) {
-				s += str1.substring(i, i + 1);
-			} else
-				continue;
+			s += str1.substring(i, i + 1);
 		}
 		for (int i = 0; i < str2.length(); i++) {
-			char t = str2.charAt(i);
-			if (65 <= t && t <= 90) {
-				s2 += str2.substring(i, i + 1);
-			} else if (97 <= t && t <= 122) {
-				s2 += str2.substring(i, i + 1);
-			} else
-				continue;
+			s2 += str2.substring(i, i + 1);
 		}
 
 		for (int i = 0; i < s.length() - 1; i++) {
 			String tmp = s.substring(i, i + 2).toLowerCase();
-			list.add(tmp);
+			check = false;
+			for (int j = 0; j < 2; j++) {
+				char t = tmp.charAt(j);
+				if (65 <= t && t <= 90) {
+					continue;
+				} else if (97 <= t && t <= 122) {
+					continue;
+				} else
+					check = true;
+			}
+			if (!check)
+				list.add(tmp);
 		}
 
 		for (int i = 0; i < s2.length() - 1; i++) {
 			String tmp = s2.substring(i, i + 2).toLowerCase();
-			list2.add(tmp);
+			check = false;
+			for (int j = 0; j < 2; j++) {
+				char t = tmp.charAt(j);
+				if (65 <= t && t <= 90) {
+					continue;
+				} else if (97 <= t && t <= 122) {
+					continue;
+				} else
+					check = true;
+			}
+			if (!check)
+				list2.add(tmp);
 		}
 		int count = 0;
+		boolean visited[] = new boolean[list2.size()];
 		for (String i : list) {
+			index = 0;
 			for (String j : list2) {
+				if (!visited[index]) {
 				if (i.equals(j)) {
+					 System.out.print(i+" ");
+					visited[index] = true;
 					count++;
 					break;
-				}
+				}}
+				index++;
 			}
 		}
-		int count2 = 0;
-		for (String i : list) {
-			for (String j : list2) {
-				if (i.equals(j)) {
-					count++;
-					break;
-				}
-			}
-		}
+
 		double t;
-		if (count == 0)
+		if (list.size() == 0 && list2.size() == 0) {
 			t = 1;
-		else
+		} else
 			t = ((double) count / (double) (list.size() + list2.size() - count));
+
 		answer = (int) (t * 65536);
 		System.out.println(answer);
 		return answer;
@@ -67,8 +78,8 @@ public class Kakao03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "handshake";
-		String s2 = "shake hands";
+		String s = "FRANCE";
+		String s2 = "french";
 		Kakao03(s, s2);
 	}
 

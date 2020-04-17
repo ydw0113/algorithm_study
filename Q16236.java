6339bfff0890c65f0ad4b5893eb68f081e0697f8
유동완 <ydw0113@naver.com>
2020-04-17 23:31:25 +0900

    스터디

--- Q16236.java
+++ Q16236.java
@@ -8,11 +8,11 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q16236 {
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, -1, 0, 1 };
-	static int n, time = 0, sum = 0, tmp = 2;
+	static int[] dx = { -1, 0, 0, 1 };
+	static int[] dy = { 0, -1, 1, 0 };
+	static int n, time = 0, sum = 0, tmp = 0, size = 2;
 	static int[][] arr, copy;
-	static boolean visited[][], flag, flag2;
+	static boolean visited[][], flag2;
 	static Queue<DOT> q = new LinkedList<DOT>();
 
 	public static void main(String[] args) throws IOException {
@@ -29,13 +29,14 @@ public class Q16236 {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 				if (arr[i][j] == 9) {
 					q.add(new DOT(i, j));
-					// visited[i][j] = true;
+					visited[i][j]=true;
+					arr[i][j]=0;
 				}
 			}
 		}
 		copyMap();
 		bfs();
-		if (tmp == 2)
+		if (size == 2)
 			time = 0;
 		System.out.println(time + "  크기" + tmp);
 
@@ -51,42 +52,56 @@ public class Q16236 {
 
 				if (nx < 0 || nx >= n || ny < 0 || ny >= n)
 					continue;
-				if (arr[nx][ny] >= tmp || visited[nx][ny])
+				if (arr[nx][ny] > size || visited[nx][ny])
 					continue;
-				copy[nx][ny] = copy[d.x][d.y] + 1;
-				if (arr[nx][ny] != 0) {
 
-					System.out.println(copy[nx][ny]);
+				if (!check()) {
+					flag2 = true;
+					break;
+				}
+				copy[nx][ny] = copy[d.x][d.y] + 1;
+				if (arr[nx][ny] > 0 && arr[nx][ny] < size) {
 					time += copy[nx][ny];
-					arr[nx][ny] = 9;
+					arr[nx][ny] = 0;
 					copyMap();
 					tmp++;
 					visited[nx][ny] = true;
+					q.clear();
 					q.add(new DOT(nx, ny));
-				} else {
+					System.out.println(nx+" "+ny+" size= "+size+" t "+time);
 					print();
 					System.out.println();
-					visited[nx][ny] = true;
-					q.add(new DOT(nx, ny));
-					check();
-					if (!flag) {
-						flag2 = true;
+					if (tmp == size) {
+						size++;
+						copyMap();
+						tmp = 0;
 						break;
 					}
-					flag = false;
+					break;
+				} else {
+					visited[nx][ny] = true;
+					q.add(new DOT(nx, ny));
 				}
 			}
 			if (flag2)
 				break;
+			
 		}
 	}
 
 	public static void print() {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				System.out.print(copy[i][j] + " ");
+				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
+		}
+	}
+	public static void tr() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				visited[i][j]=false;
+			}
 		}
 	}
 
@@ -101,13 +116,23 @@ public class Q16236 {
 		}
 	}
 
-	public static void check() {
+	public static boolean check() {
+		int count = 0;
+		boolean flag = false;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (arr[i][j] < 9 && arr[i][j] >= 1)
+				if (arr[i][j] <= 6 && arr[i][j] >= 1) {
 					flag = true;
+					if (arr[i][j] < size) {
+						count++;
+					}
+				}
 			}
 		}
+		if (count == 0)
+			return false;
+		else
+			return flag;
 	}
 
 }
